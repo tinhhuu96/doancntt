b4c1addc4b492f9a8fa6c06e353e97155256ac0a
tranluong96 <luongtvit96@gmail.com>
2017-12-09 09:13:17 +0700

    commit manage promotion

--- routes/web.php
+++ routes/web.php
@@ -182,6 +182,14 @@ Route::group(['namespace' => 'AdminController','prefix' => 'adminpc', 'middlewar
         ]);
 
     });
+
+    Route::group(['prefix' => 'Promotion'], function () {
+        Route::get('',[
+            'uses'  => 'PromotionController@index',
+            'as'    => 'promotion.index'
+        ]);
+    });
+
     Route::group(['prefix' => 'Category'], function () {
         Route::get('',[
             'uses'  => 'CateController@index',
