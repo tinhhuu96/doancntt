5cb97dd17235452f726cd196a27f6a29f325bd8c
tranluong96 <luongtvit96@gmail.com>
2018-01-13 01:33:32 +0700

    convert checkout

--- resources/views/layouts/cart/checkout.blade.php
+++ resources/views/layouts/cart/checkout.blade.php
@@ -11,8 +11,27 @@
               <li class="active">Check out</li>
             </ol>
         </div><!--/breadcrums-->
-
-        <div class="shopper-informations" >
+        <div class="shopper-informations">
+            <div class="row" style=" margin-bottom: 15px; ">
+                <div class="col-sm-6">
+                    <div class="shopper-info">
+                        <label>chọn cách mua hàng</label>
+                        <div id="accordion" role="tablist">
+                          <div class="card">
+                            <div class="card-header btn" style="background-color: #FE980F; width: 300px;" role="tab" id="headingTwo">
+                              <h5 class="mb-0">
+                                <a class="collapsed" data-toggle="collapse" href="#collapseTwo" role="button" aria-expanded="false" aria-controls="collapseTwo" style="color: #fff;">
+                                  Mua hàng không qua đăng nhập
+                                </a>
+                              </h5>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="shopper-informations collapse" id="collapseTwo" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion" >
             <div class="row" style=" margin-bottom: 15px; ">
                 <div class="col-sm-6">
                     <div class="shopper-info">
@@ -35,7 +54,80 @@
                 </div>
             </div>
         </div>
+        <hr>
+        <div class="card-header btn" style="background-color: #FE980F; width: 300px;" role="tab" id="headingThree">
+          <h5 class="mb-0">
+            <a class="collapsed" data-toggle="collapse" href="#collapseThree" role="button" aria-expanded="false" aria-controls="collapseThree" style="color: #fff;">
+              Đăng nhập mua hàng
+            </a>
+          </h5>
+        </div>
+        <br>
+        <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+           <div class="row">
+                <div class="col-md-10 col-md-offset-2" style="margin-left: 0px !important">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Login</div>
 
+                        <div class="panel-body">
+                            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                                {{ csrf_field() }}
+
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <label for="password" class="col-md-4 control-label">Password</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control" name="password" required>
+
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-md-8 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Login
+                                        </button>
+
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            Forgot Your Password?
+                                        </a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </div>
+      <br>
     </div>
 </section>
 @stop
